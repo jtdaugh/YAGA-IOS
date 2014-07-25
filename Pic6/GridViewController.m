67891b1a6b0ce814fef7d530ca09da7b587ca8bc
Raj Vir <raj@rjvir.com>
2014-07-24 21:04:35 -0700

    crashlytics stuff

--- Pic6/GridViewController.m
+++ Pic6/GridViewController.m
@@ -14,6 +14,7 @@
 #import "OverlayViewController.h"
 #import "CliqueViewController.h"
 #import "OnboardingNavigationController.h"
+#import <Crashlytics/Crashlytics.h>
 
 @interface GridViewController ()
 @end
@@ -59,6 +60,7 @@
     self.cameraAccessories = [[NSMutableArray alloc] init];
     [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayAndRecord error:nil];
 //    [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayback withOptions:AVAudioSessionCategoryOptionMixWithOthers error:nil];
+    [Crashlytics setUserIdentifier:[self humanName]];
 
     [self initOverlay];
     [self initGridView];
