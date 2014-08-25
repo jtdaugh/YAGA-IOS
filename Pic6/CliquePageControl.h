077e7b08db3f6f9eaff75f90c1212e95f4f4b05f
Veeral Patel <v@veeralpatel.com>
2014-08-25 17:10:46 -0400

    custom pagecontrol wip

---
+++ Pic6/CliquePageControl.h
@@ -0,0 +1,17 @@
+//
+//  CliquePageControl.h
+//  Pic6
+//
+//  Created by Veeral Patel on 8/24/14.
+//  Copyright (c) 2014 Raj Vir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CliquePageControl : UIView
+
+@property (nonatomic, strong) NSString *title;
+@property (nonatomic) NSInteger currentPage;
+@property (nonatomic) NSInteger numberOfPages;
+
+@end
---
+++ Pic6/CliquePageControl.m
@@ -0,0 +1,36 @@
+//
+//  CliquePageControl.m
+//  Pic6
+//
+//  Created by Veeral Patel on 8/24/14.
+//  Copyright (c) 2014 Raj Vir. All rights reserved.
+//
+
+#import "CliquePageControl.h"
+
+@interface CliquePageControl ()
+
+@property (nonatomic, strong) UIPageControl *pageControl;
+
+@end
+
+@implementation CliquePageControl
+
+- (instancetype)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
+        self.backgroundColor = [UIColor blueColor];
+        
+        UIPageControl *pageControl = [[UIPageControl alloc] init];
+        pageControl.translatesAutoresizingMaskIntoConstraints = NO;
+        pageControl.userInteractionEnabled = NO;
+        self.pageControl = pageControl;
+        
+        NSDictionary *views = NSDictionaryOfVariableBindings(pageControl);
+        [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[pageControl]|" options:0 metrics:nil views:views]];
+        []
+    }
+    return self;
+}
+
+@end
