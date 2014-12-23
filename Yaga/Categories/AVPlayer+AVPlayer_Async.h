d5f130f561c0fd82345f2a06b710196d0c6b4627
valkov <valentinkovalski@gmail.com>
2014-12-23 18:12:14 +0200

    file deleted by mistake added back

---
+++ Yaga/Categories/AVPlayer+AVPlayer_Async.h
@@ -0,0 +1,15 @@
+//
+//  AVPlayer+AVPlayer_Async.h
+//  Pic6
+//
+//  Created by Raj Vir on 6/5/14.
+//  Copyright (c) 2014 Raj Vir. All rights reserved.
+//
+
+#import <AVFoundation/AVFoundation.h>
+
+@interface AVPlayer (AVPlayer_Async)
+- (void)asyncPlay;
+- (void)setLooping;
+- (void)removeObservers;
+@end
---
+++ Yaga/Categories/AVPlayer+AVPlayer_Async.m
@@ -0,0 +1,49 @@
+//
+//  AVPlayer+AVPlayer_Async.m
+//  Pic6
+//
+//  Created by Raj Vir on 6/5/14.
+//  Copyright (c) 2014 Raj Vir. All rights reserved.
+//
+
+#import "AVPlayer+AVPlayer_Async.h"
+
+@implementation AVPlayer (AVPlayer_Async)
+
+- (void)asyncPlay {
+    [self addObserver:self forKeyPath:@"status"
+                                          options:0 context:nil];
+}
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object
+                        change:(NSDictionary *)change context:(void *)context {
+
+    if(object == self && [keyPath isEqualToString:@"status"]){
+        if(self.status == AVPlayerStatusReadyToPlay){
+            [self play];
+            [self removeObserver:self forKeyPath:@"status"];
+        }
+    }
+}
+
+- (void)setLooping {
+    // set looping
+    
+    [self setActionAtItemEnd:AVPlayerActionAtItemEndNone];
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(playerItemDidReachEnd:)
+                                                 name:AVPlayerItemDidPlayToEndTimeNotification
+                                               object:[self currentItem]];
+    
+}
+
+- (void)playerItemDidReachEnd:(NSNotification *)notification {
+    AVPlayerItem *p = [notification object];
+    [p seekToTime:kCMTimeZero];
+}
+
+- (void)removeObservers {
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
+@end
