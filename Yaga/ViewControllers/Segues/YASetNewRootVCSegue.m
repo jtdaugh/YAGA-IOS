ff3d5c2645e9a96d5514e35fe1e55435d58e5aa9
Iegor Shapanov <hiiegor@gmail.com>
2014-12-30 16:42:51 +0200

    Removed back button from smsverification screen

---
+++ Yaga/ViewControllers/Segues/YASetNewRootVCSegue.m
@@ -0,0 +1,27 @@
+//
+//  YASetNewRootVCSegue.m
+//  Yaga
+//
+//  Created by Iegor on 12/30/14.
+//  Copyright (c) 2014 Raj Vir. All rights reserved.
+//
+#import "AppDelegate.h"
+#import "YASetNewRootVCSegue.h"
+
+@implementation YASetNewRootVCSegue
+- (void) perform
+{
+    UIViewController *dst = (UIViewController *) self.destinationViewController;
+
+    AppDelegate *appDelegate = [[UIApplication sharedApplication] delegate];
+    appDelegate.window.rootViewController = dst;
+   
+    CATransition *animation = [CATransition animation];
+    [animation setDuration:0.4];
+    [animation setType:kCATransitionPush];
+    [animation setSubtype:kCATransitionFromTop];
+    [animation setTimingFunction:[CAMediaTimingFunction  functionWithName:kCAMediaTimingFunctionEaseInEaseOut]];
+    
+    [[dst.view layer] addAnimation:animation forKey:nil];
+}
+@end
