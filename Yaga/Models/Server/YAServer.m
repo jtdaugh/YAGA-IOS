082e60b0ffd8d4471170c6b435a00d53826cb6cb
valkov <valentinkovalski@valentinkovalski.local>
2015-01-19 16:44:18 +0200

    working push token registration

--- Yaga/Models/Server/YAServer.m
+++ Yaga/Models/Server/YAServer.m
@@ -25,7 +25,7 @@
 #define API_ENDPOINT @"/yaga/api/v1"
 
 #define API_USER_PROFILE_TEMPLATE           @"%@/user/profile/"
-#define API_USER_DEVICE_TEMPLATE            @"%@/auth/device/"
+#define API_USER_DEVICE_TEMPLATE            @"%@/user/device/"
 
 #define API_AUTH_TOKEN_TEMPLATE             @"%@/auth/obtain/"
 #define API_AUTH_BY_SMS_TEMPLATE            @"%@/auth/request/"
@@ -429,13 +429,12 @@
 
 - (void)registerDeviceTokenWithCompletion:(responseBlock)completion {
     NSAssert(self.token.length, @"token not set");
-    NSAssert([YAUser currentUser].phoneNumber.length, @"phone number not set");
     NSAssert([YAUser currentUser].deviceToken.length, @"device token not set");
     
     NSString *api = [NSString stringWithFormat:API_USER_DEVICE_TEMPLATE, self.base_api];
     
     NSDictionary *parameters = @{
-                                 @"vendor": [YAUser currentUser].phoneNumber,
+                                 @"vendor": @"IOS",
                                  @"token": [YAUser currentUser].deviceToken
                                  };
     
@@ -475,20 +474,25 @@
     return [self.reachability isReachable];
 }
 
+- (void)registerDeviceTokenIfNeeded {
+    if(![YAUser currentUser].deviceToken.length)
+        return;
+    //every day? maybe every month?
+    NSTimeInterval lastRegistrationDate = [[[NSUserDefaults standardUserDefaults] objectForKey:YA_LAST_DEVICE_TOKEN_SYNC_DATE] timeIntervalSinceNow];
+    if(!lastRegistrationDate || fabs(lastRegistrationDate) > 24 * 60 * 60) {
+        [self registerDeviceTokenWithCompletion:^(id response, NSError *error) {
+            if(error) {
+                [YAUtils showNotification:[NSString stringWithFormat:@"Can't register device token. %@", error.localizedDescription] type:AZNotificationTypeError];
+            }
+        }];
+    }
+}
 - (void)sync {
     NSLog(@"YAServer:sync, serverUp: %@", self.serverUp ? @"Yes" : @"No");
     
     if([[YAUser currentUser] loggedIn] && self.token.length && self.serverUp) {
-        //register token every day
-        if([YAUser currentUser].deviceToken.length) {
-            if([[[NSUserDefaults standardUserDefaults] objectForKey:YA_LAST_DEVICE_TOKEN_SYNC_DATE] timeIntervalSinceNow] > 24 * 60 * 60) {
-                [self registerDeviceTokenWithCompletion:^(id response, NSError *error) {
-                    if(error) {
-                        [YAUtils showNotification:[NSString stringWithFormat:@"Can't register device token. %@", error.localizedDescription] type:AZNotificationTypeError];
-                    }
-                }];
-            }
-        }
+        
+        [self registerDeviceTokenIfNeeded];
         
         //read updates from server
         [YAGroup updateGroupsFromServerWithCompletion:^(NSError *error) {
