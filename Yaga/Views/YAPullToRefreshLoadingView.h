023004f5db3c40ee05142c2950e714badd65341b
valkov <valentinkovalski@valentinkovalski.local>
2015-02-03 06:31:31 +0200

    experiment 1

---
+++ Yaga/Views/YAPullToRefreshLoadingView.h
@@ -0,0 +1,13 @@
+//
+//  YAPullToRefreshLoadingView.h
+//  Yaga
+//
+//  Created by valentinkovalski on 2/3/15.
+//  Copyright (c) 2015 Raj Vir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YAPullToRefreshLoadingView : UIView
+
+@end
