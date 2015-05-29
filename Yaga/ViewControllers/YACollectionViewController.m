aefd98b5f95172c46929a0016fe6a8a449dc7e09
valkov <valentinkovalski@valentinkovalski.local>
2015-05-30 00:15:46 +0300

    gif cache removed - scrolling lag fixed

--- Yaga/ViewControllers/YACollectionViewController.m
+++ Yaga/ViewControllers/YACollectionViewController.m
@@ -23,8 +23,6 @@
 
 #import "YANotificationView.h"
 
-#import "YAImageCache.h"
-
 @protocol GridViewControllerDelegate;
 
 static NSString *YAVideoImagesAtlas = @"YAVideoImagesAtlas";
@@ -598,36 +596,7 @@ static NSString *cellID = @"Cell";
     }
     
     [[YAAssetsCreator sharedCreator] enqueueAssetsCreationJobForVisibleVideos:videos invisibleVideos:nil];
-    
-    [self precacheGifsStartingFromIndex:[visibleVideoIndexes.lastObject integerValue]];
 }
-
-- (void)precacheGifsStartingFromIndex:(NSUInteger)startingIndex {
-    
-    NSUInteger currentIndex = startingIndex + 1;
-    
-    const NSUInteger defaultPrecacheCount = 30;
-    while (currentIndex < [YAUser currentUser].currentGroup.videos.count && (currentIndex - startingIndex <= defaultPrecacheCount)) {
-        YAVideo *video = [YAUser currentUser].currentGroup.videos[currentIndex];
-        
-        NSString *fileName = video.gifFilename;
-        if(fileName.length && ![[YAImageCache sharedCache] objectForKey:fileName]) {
-            dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-                
-                NSURL *dataURL = [YAUtils urlFromFileName:fileName];
-                NSData *fileData = [NSData dataWithContentsOfURL:dataURL];
-                FLAnimatedImage *image = [[FLAnimatedImage alloc] initWithAnimatedGIFData:fileData];
-                
-                if(image) {
-                    [[YAImageCache sharedCache] setObject:image forKey:fileName];
-                    DLog(@"gif precached");
-                }
-            });
-        }
-        currentIndex++;
-    }
-}
-
 
 - (void)enqueueAssetsCreationJobsStartingFromVideoIndex:(NSUInteger)initialIndex {
     NSUInteger maxCount = self.paginationThreshold;
