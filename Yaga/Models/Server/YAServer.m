b7c87755876c1fa0c534b85799b7837c070c2867
Jesse <jesse@Jesses-MacBook-Pro.local>
2015-05-30 15:33:23 -0700

    firebasinnnn

--- Yaga/Models/Server/YAServer.m
+++ Yaga/Models/Server/YAServer.m
@@ -93,7 +93,7 @@
         
         [Firebase setOption:@"persistence" to:@YES];
         
-        self.firebase = [[Firebase alloc] initWithUrl:@"https://yaga.firebaseio.com/video_data_dev"];
+        self.firebase = [[Firebase alloc] initWithUrl:@"https://yaga.firebaseio.com/video_data_cnc"];
         
         [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(willDeleteVideo:)  name:VIDEO_WILL_DELETE_NOTIFICATION  object:nil];
         
