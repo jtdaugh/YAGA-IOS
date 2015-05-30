567d63c29116d0056421eb05ce2034a7a9977fca
Raj Vir <raj@rjvir.com>
2015-05-29 19:04:07 -0700

    positioning

--- Yaga/Views/YACommentsCell.m
+++ Yaga/Views/YACommentsCell.m
@@ -23,16 +23,33 @@
         CGFloat initialUsernameWidth = 100, initialHeight = 24;
         self.usernameLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, initialUsernameWidth, initialHeight)];
         self.usernameLabel.textColor = PRIMARY_COLOR;
-        self.usernameLabel.font = [UIFont boldSystemFontOfSize:15.f];
+        self.usernameLabel.font = [UIFont boldSystemFontOfSize:18.f];
+        
+        self.usernameLabel.shadowColor = [UIColor blackColor];
+        self.usernameLabel.shadowOffset = CGSizeMake(0.5, 0.5);
+        
+//        
+//        self.commentsTextView.layer.shadowColor = [UIColor blackColor].CGColor;
+//        self.commentsTextView.layer.shadowOffset = CGSizeMake(1.0, 1.0);
+//        self.commentsTextView.layer.shadowOpacity = 1.0;
+//        self.commentsTextView.layer.shadowRadius = 0.0f;
+        
         [self addSubview:self.usernameLabel];
+
         self.commentsTextView = [[UITextView alloc] initWithFrame:CGRectMake(initialUsernameWidth, 0, self.frame.size.width - initialUsernameWidth, initialHeight)];
         self.commentsTextView.textContainer.lineFragmentPadding = 0;
         self.commentsTextView.textContainerInset = UIEdgeInsetsZero;
         self.commentsTextView.textColor = [UIColor whiteColor];
         self.commentsTextView.textContainerInset = UIEdgeInsetsZero;
-        self.commentsTextView.font = [UIFont systemFontOfSize:14.f];
+        self.commentsTextView.font = [UIFont systemFontOfSize:18.f];
         self.commentsTextView.backgroundColor = [UIColor clearColor];
         self.commentsTextView.scrollEnabled = NO;
+        
+        self.commentsTextView.layer.shadowColor = [UIColor blackColor].CGColor;
+        self.commentsTextView.layer.shadowOffset = CGSizeMake(0.5, 0.5);
+        self.commentsTextView.layer.shadowOpacity = 1.0;
+        self.commentsTextView.layer.shadowRadius = 0.0f;
+        
         [self addSubview:self.commentsTextView];
         
 //        [self.usernameLabel setBackgroundColor:[UIColor greenColor]];
@@ -53,8 +70,8 @@
     userFrame.size = userSize;
     self.usernameLabel.frame = userFrame;
     CGRect commentFrame = self.commentsTextView.frame;
-    commentFrame.origin.x = userSize.width + 4;
-    commentFrame.size.width = VIEW_WIDTH - (userSize.width + 12);
+    commentFrame.origin.x = userSize.width + 6.0f;
+    commentFrame.size.width = VIEW_WIDTH - (userSize.width + 6.0f);
 //    commentFrame.origin.x = userWidth + 10;
 //    commentFrame.size.width = self.frame.size.width - (userWidth + 10);
     self.commentsTextView.frame = commentFrame;
@@ -64,15 +81,15 @@
     // should actually implement this
     UILabel *dummy = [[UILabel alloc] init];
     dummy.text = username;
-    dummy.font = [UIFont boldSystemFontOfSize:14.f];
+    dummy.font = [UIFont boldSystemFontOfSize:18.f];
     CGSize userSize = [dummy sizeThatFits:CGSizeMake(VIEW_WIDTH/2, CGFLOAT_MAX)];
     
     UITextView *dummyTextView = [[UITextView alloc] init];
-    dummyTextView.font = [UIFont boldSystemFontOfSize:14.f];
+    dummyTextView.font = [UIFont boldSystemFontOfSize:18.f];
     dummyTextView.textContainer.lineFragmentPadding = 0;
     dummyTextView.textContainerInset = UIEdgeInsetsZero;
     dummyTextView.text = comment;
-    CGFloat commentWidth = VIEW_WIDTH - (userSize.width + 12);
+    CGFloat commentWidth = VIEW_WIDTH - (userSize.width + 6.0f);
 
     CGSize commentSize = [dummyTextView sizeThatFits:CGSizeMake(commentWidth, CGFLOAT_MAX)];
     
