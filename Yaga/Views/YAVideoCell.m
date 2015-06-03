5f4527c785b14e49e4b5f1ff382a7c920df72a27
Raj Vir <raj@rjvir.com>
2015-06-02 17:07:30 -0700

    caption frame

--- Yaga/Views/YAVideoCell.m
+++ Yaga/Views/YAVideoCell.m
@@ -71,7 +71,7 @@
 
         self.backgroundView = self.loaderView;
         
-        self.username = [[UILabel alloc] initWithFrame:CGRectMake(5, self.bounds.size.height - 30, self.bounds.size.width - 10, 30)];
+        self.username = [[UILabel alloc] initWithFrame:CGRectMake(self.bounds.size.width/2, self.bounds.size.height - 30, self.bounds.size.width/2 - 5, 30)];
         [self.username setTextAlignment:NSTextAlignmentRight];
         [self.username setMinimumScaleFactor:0.5];
         [self.username setAdjustsFontSizeToFitWidth:YES];
