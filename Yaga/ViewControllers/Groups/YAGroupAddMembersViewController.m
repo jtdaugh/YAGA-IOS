ff586d5870cbded2a39f3dcba582643ab1b1d60b
valkov <valentinkovalski@77-52-12-4.dialup.umc.net.ua>
2015-06-17 14:10:33 +0300

    crash on add members screen fixed

--- Yaga/ViewControllers/Groups/YAGroupAddMembersViewController.m
+++ Yaga/ViewControllers/Groups/YAGroupAddMembersViewController.m
@@ -501,16 +501,17 @@
     NSMutableArray *memberPhones = [NSMutableArray new];
     for(YAContact *contact in self.existingGroup.members) {
         
+        NSString *memberPhone = [contact readableNumber];
+        if(!memberPhone.length)
+            continue;
+        
         NSDictionary *item = @{nCompositeName:[NSString stringWithFormat:@"%@ %@", contact.firstName, contact.lastName],
-                               nPhone:[contact readableNumber],
+                               nPhone:memberPhone,
                                nFirstname: [NSString stringWithFormat:@"%@", contact.firstName],
                                nLastname:  [NSString stringWithFormat:@"%@", contact.lastName],
                                nRegistered:[NSNumber numberWithBool:contact.registered]};
         [self.selectedContacts addObject:item];
-        
-        NSString *memberPhone = [contact readableNumber];
-        if(memberPhone.length)
-            [memberPhones addObject:memberPhone];
+        [memberPhones addObject:memberPhone];
     }
     
     self.filteredContacts = [[self.deviceContacts filteredArrayUsingPredicate:[NSPredicate predicateWithFormat:@"NOT (%K IN %@)", nPhone, memberPhones]] mutableCopy];
