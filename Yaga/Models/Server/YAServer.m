a06a9e7009ca166e1bf982bd2b58a1ba0cfa3424
valkov <valentinkovalski@valentinkovalski.local>
2015-06-30 16:39:48 +0300

    test user agent committed by mistake reverted

--- Yaga/Models/Server/YAServer.m
+++ Yaga/Models/Server/YAServer.m
@@ -513,8 +513,6 @@
     NSString *api = [NSString stringWithFormat:API_GROUP_POSTS_TEMPLATE, self.base_api, serverGroupId];
     NSString *videoLocalId = [video.localId copy];
     
-    [self.jsonOperationsManager.requestSerializer setValue:@"YAGA IOS 1" forHTTPHeaderField:@"user-agent"];
-    
     [self.jsonOperationsManager POST:api
             parameters:nil
                success:^(AFHTTPRequestOperation *operation, id responseObject) {
