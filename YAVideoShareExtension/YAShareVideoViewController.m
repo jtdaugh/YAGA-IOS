97ae122af7d6770e8205e93531ba058cef63ad21
Christopher Wendel <chriwend@umich.edu>
2015-07-16 12:06:14 -0700

    Adding video playing logic to YAShareVideoViewController

--- YAVideoShareExtension/YAShareVideoViewController.m
+++ YAVideoShareExtension/YAShareVideoViewController.m
@@ -7,19 +7,53 @@
 //
 
 #import "YAShareVideoViewController.h"
+#import "YAVideoPlayerView.h"
 
 @interface YAShareVideoViewController ()
+
+@property (nonatomic, strong) YAVideoPlayerView *playerView;
 
 @end
 
 @implementation YAShareVideoViewController
 
+#pragma mark - View setup
+
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    _playerView = [YAVideoPlayerView new];
+    [self.view addSubview:self.playerView];
 }
 
 - (void)viewDidAppear:(BOOL)animated {
     [super viewDidAppear:animated];
 }
 
+#pragma mark - Video
+
+- (void)prepareVideoForPlaying:(NSString *)filename {
+    NSURL *movUrl = [YAShareVideoViewController urlFromFileName:filename];
+    
+    if (filename.length) {
+        self.playerView.URL = movUrl;
+    } else {
+        self.playerView.URL = nil;
+    }
+    
+    self.playerView.frame = self.view.bounds;
+}
+
++ (NSURL *)urlFromFileName:(NSString *)fileName {
+    if(!fileName.length)
+        return nil;
+    
+    NSString *path = [[YAShareVideoViewController cachesDirectory] stringByAppendingPathComponent:fileName];
+    return [NSURL fileURLWithPath:path];
+}
+
++ (NSString *)cachesDirectory {
+    NSArray *cachePaths = NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES);
+    return cachePaths[0];
+}
 @end
