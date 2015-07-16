ec54d5585f6355de23a64324fdb4348fd519d18f
Christopher Wendel <chriwend@umich.edu>
2015-07-16 00:50:04 -0700

    Beginnings of share extension

---
+++ YAVideoShareExtension/ShareViewController.h
@@ -0,0 +1,14 @@
+//
+//  ShareViewController.h
+//  YAVideoShareExtension
+//
+//  Created by Christopher Wendel on 7/16/15.
+//  Copyright (c) 2015 Raj Vir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Social/Social.h>
+
+@interface ShareViewController : SLComposeServiceViewController
+
+@end
