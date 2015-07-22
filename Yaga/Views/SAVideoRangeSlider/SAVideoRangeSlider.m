5a0b653354edab04944358f6a937852ef91d8803
Valentin Kovalski <valentinkovalski@gmail.com>
2015-07-23 02:29:06 +0300

    trimming control fix

--- Yaga/Views/SAVideoRangeSlider/SAVideoRangeSlider.m
+++ Yaga/Views/SAVideoRangeSlider/SAVideoRangeSlider.m
@@ -389,13 +389,14 @@
             
             NSMutableArray *allTimes = [[NSMutableArray alloc] init];
             
-            dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-                int time4Pic = 0;
-                // Bug iOS7 - generateCGImagesAsynchronouslyForTimes
-                __block int prefreWidth=0;
-                for (__block int i=1, ii=1; i<picsCnt; i++){
-                    time4Pic = i*picWidth;
-                    
+            
+            int time4Pic = 0;
+            // Bug iOS7 - generateCGImagesAsynchronouslyForTimes
+            __block int prefreWidth=0;
+            for (__block int i=1, ii=1; i<picsCnt; i++){
+                time4Pic = i*picWidth;
+                
+                dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
                     CMTime timeFrame = CMTimeMakeWithSeconds(weakSelf.durationSeconds*time4Pic/weakSelf.bgView.frame.size.width, 600);
                     
                     [allTimes addObject:[NSValue valueWithCMTime:timeFrame]];
@@ -441,9 +442,9 @@
                         
                         CGImageRelease(halfWayImage);
                     });
-                    
-                }
-            });
+                });
+            }
+            
         });
     });
 }
