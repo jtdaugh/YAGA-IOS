a3db9260416f0d68000200225e620cc04f26a41a
Raj Vir <raj@rjvir.com>
2015-08-21 22:10:41 -0700

    initial screens

---
+++ Yaga/ViewControllers/Onboarding/YASplashViewController.h
@@ -0,0 +1,13 @@
+//
+//  YASplashViewController.h
+//  Yaga
+//
+//  Created by Raj Vir on 8/21/15.
+//  Copyright (c) 2015 Raj Vir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YASplashViewController : UIViewController
+
+@end
